7a21a5ce77e64842da6e6201478a57d6ba0ebbc8
Ahda-Rizqi <AhdaRizqi1199@gmail.com>
2025-11-30 11:18:11 +0700

    fix : deploy fe #7

--- mychapter-frontend/src/main.jsx
+++ mychapter-frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./app.jsx";
+import App from "./app";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 import { ThemeProvider } from "./features/ThemeProvider";
